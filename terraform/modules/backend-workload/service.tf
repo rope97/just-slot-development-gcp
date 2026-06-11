3addb33859ca0c5a14c19672299943285e759a05
Danica <ddimitrijevic@dedicatedadmin.com>
2026-06-11 15:18:34 +0200

    fix ci pipelines

--- terraform/modules/backend-workload/service.tf
+++ terraform/modules/backend-workload/service.tf
@@ -8,7 +8,7 @@ resource "kubernetes_service" "backend" {
       app = "game-backend"
     }
 
-    type = "LoadBalancer"
+    type = "ClusterIP"
     port {
       port        = 80
       target_port = 3000
