97cb796cdfd930418ba14b97570cc4784cf4b78f
Danica <ddimitrijevic@dedicatedadmin.com>
2026-06-12 11:41:28 +0200

    fix terraform fmt

--- terraform/environments/production/main.tf
+++ terraform/environments/production/main.tf
@@ -1,27 +1,27 @@
 provider "google" {
-project = "just-slots-499010"
-region  = "us-central1"
+  project = "just-slots-499010"
+  region  = "us-central1"
 }
 
 provider "kubernetes" {
-config_path = "~/.kube/config"
+  config_path = "~/.kube/config"
 }
 
 module "registry" {
-source = "../../modules/registry"
+  source = "../../modules/registry"
 }
 
 module "backend_workload" {
-source = "../../modules/backend-workload"
+  source = "../../modules/backend-workload"
 
-env   = "production"
-image = "europe-west1-docker.pkg.dev/just-slots-499010/game-backend/api:prod"
-host  = "api-slots.duckdns.org"
+  env   = "production"
+  image = "europe-west1-docker.pkg.dev/just-slots-499010/game-backend/api:prod"
+  host  = "api-slots.duckdns.org"
 }
 
 module "static_site" {
-source = "../../modules/static-site"
+  source = "../../modules/static-site"
 
-bucket_name = "just-slots-fe-prod"
-location    = "US"
+  bucket_name = "just-slots-fe-prod"
+  location    = "US"
 }
